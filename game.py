c0337d8f1322568eaf69b233bfdfb852879adc39
Litrid <65836019+Litrid@users.noreply.github.com>
2023-04-18 15:37:26 -0400

    Initial Commit

---
+++ game.py
@@ -0,0 +1,30 @@
+from setup import *
+from grid import Grid
+
+
+class Game:
+
+	def __init__(self):
+		self.grid = Grid(10, 10, 5)
+		self.game_over = False
+
+	def update(self, delta):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return COMMAND_EXIT
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_r:
+					return COMMAND_RESTART
+
+		mouse_pos = pygame.mouse.get_pos()
+		mouse_button = pygame.mouse.get_pressed()
+		if self.grid.update(delta, mouse_pos, mouse_button):
+			return COMMAND_LOSE
+		if self.grid.win():
+			return COMMAND_WIN
+
+		return COMMAND_NONE
+
+	def draw(self, screen):
+		screen.fill(BLACK)
+		self.grid.draw(screen)
